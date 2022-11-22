b82da9c2ae25183dc227e3e98d86c3dda3d8d9fe
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-22 15:23:34 +0530

    Create MaxString.java

---
+++ MaxString.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.generics;
+
+public class MaxString {
+    void maxValue(String val1, String val2, String val3){
+            if((val1.compareTo(val2)) >=0 && (val1.compareTo(val3)) >=0)
+                System.out.println(val1+ " is the largest: ");
+
+            else if ((val2.compareTo(val1)) >=0 && (val2.compareTo(val3)) >=0) {
+                System.out.println(val2+" is largest: ");
+            }
+            else{
+                System.out.println(val3+" is largest: ");
+
+            }
+
+    }
+}
